61a1cd57d2d47e8421fb826021d440bb64009c9a
Daniel Maldonado <daniel_dlcmaldonado@hotmail.com>
2023-08-07 12:08:43 -0600

    se elimina código innecesario (comentarios, etc.)

--- js/rubrica.js
+++ js/rubrica.js
@@ -27,18 +27,3 @@ document.querySelectorAll(".score-button").forEach((element) => {
 
     })
 })
-
-///////////////////////// PREV CODE
-
-// function updateScore(score) {
-//     // Restaurar el estilo de los botones de la fila actual
-//     const buttons = document.querySelectorAll('.score-button');
-//     buttons.forEach(button => button.classList.remove('active'));
-
-//     // Resaltar el botón seleccionado
-//     event.target.classList.add('active');
-
-//     // Obtener el puntaje seleccionado y actualizar el puntaje total
-//     totalScore = totalScore - parseInt(document.getElementById('total-score').innerText) + score;
-//     document.getElementById('total-score').innerText = totalScore;
-// }
